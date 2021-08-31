33c3384f22d54eda1bc311a3ac2d6a59d4e48639
Constantin Alupoaie <constantin.alupoaie@gmail.com>
2021-09-01 00:00:05 +0300

    working on validation

--- TradingApp/ClientApp/src/pages/createTrade.page.tsx
+++ TradingApp/ClientApp/src/pages/createTrade.page.tsx
@@ -3,77 +3,105 @@ import { Formik, Field, Form, FormikHelpers } from "formik";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { TradesApi, CreateTradeRequest } from "./api/trades.api";
+import { useHistory } from "react-router";
+import * as Yup from 'yup';
 
 require('react-datepicker/dist/react-datepicker.css')
-
 
 export function CreateTrade() {
     return <Test />
 }
 
 type Values = {
-    firstName: string;
-    lastName: string;
-    email: string;
-    startDate: Date
+    securityCode: string,
+    sequenceNumberLength: number,
+    sequenceNumber: string,
+    price: number,
+    date: Date
 }
 
 const initialValues: Values = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    startDate: new Date()
+    securityCode: "",
+    sequenceNumberLength: 0,
+    sequenceNumber: "",
+    price: 0,
+    date: new Date()
 };
 
+const ValidationSchema = Yup.object().shape({
+    securityCode: Yup.string()
+        .length(3, "exactly 3")
+        .required('Required'),
+    sequenceNumberLength: Yup.number()
+        .required("Required"),
+    sequenceNumber: Yup.string()
+        .required('Required'),
+    price: Yup.number()
+        .required('Required'),
+    date: Yup.date()
+        .required('Required')
+});
+
 const Test = () => {
+    const history = useHistory();
+
     return (
         <div>
             <Formik
                 initialValues={initialValues}
+                //validationSchema={ValidationSchema}
                 onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
                     setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
                         setSubmitting(false);
+                        history.goBack();
                     }, 500);
                 }}
             >
-                {({ values, setFieldValue }) => (
+                {({ values, setFieldValue, errors }) => (
                     <Form>
-                        <label htmlFor="firstName">Security Code</label>
+                        <label htmlFor="securityCode">Security Code</label>
                         <Field
-                            id="firstName"
-                            name="firstName"
-                            placeholder="John"
+                            id="securityCode"
+                            name="securityCode"
+                            placeholder="ABC"
                         />
-                        <br/>
+                        <div>{errors.securityCode}</div>
+                        <br />
 
-                        <label htmlFor="lastName">Trade Price</label>
+                        <label htmlFor="sequenceNumberLength">X</label>
                         <Field
-                            id="lastName"
-                            name="lastName"
-                            placeholder="Doe"
+                            id="sequenceNumberLength"
+                            name="sequenceNumberLength"
+                            placeholder="X = ?"
                         />
-                        <br/>
+                        <br />
 
-                        <label htmlFor="email">Email</label>
+                        <label htmlFor="sequenceNumber">SequenceNumber</label>
                         <Field
-                            id="email"
-                            name="email"
-                            placeholder="john@acme.com"
-                            type="email"
+                            id="sequenceNumber"
+                            name="sequenceNumber"
+                            placeholder=""
                         />
-                        <br/>
+                        <br />
+
+                        <label htmlFor="price">Price</label>
+                        <Field
+                            id="price"
+                            name="price"
+                            placeholder="price"
+                        />
+                        <br />
 
                         <label htmlFor="date">Date</label>
                         <DatePicker
-                            selected={values.startDate}
+                            selected={values.date}
                             dateFormat="MMMM d, yyyy"
                             className="form-control"
                             name="startDate"
-                            onChange={date => setFieldValue('startDate', date)}
+                            onChange={date => setFieldValue('date', date)}
                         />
+                        <br />
 
-                        <br/>
                         <button type="submit">Submit</button>
                     </Form>
                 )}
